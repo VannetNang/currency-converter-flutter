c072cced1fafd3ea1e36d9a1a8e4b7d165bf0072
VannetNang <NangVannet59@gmail.com>
2025-10-01 20:04:44 +0700

    Initial the Currency Converter App

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget {
+  const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Currency Converter'),
+            TextField(
+              keyboardType: TextInputType.number,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
