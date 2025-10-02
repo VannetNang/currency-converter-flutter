f0c755587aad779e2c7acf79524f1f1f71792509
VannetNang <NangVannet59@gmail.com>
2025-10-02 11:41:59 +0700

    - Finished the currency converter UI

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -6,13 +6,67 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text(
+          'Currency Converter',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Currency Converter'),
-            TextField(
-              keyboardType: TextInputType.number,
+            Padding(padding: EdgeInsets.only(bottom: 10)),
+            Text(
+              '0',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 14),
+              child: TextField(
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                decoration: InputDecoration(
+                  hintText: 'Please enter the amount of number',
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 14),
+              child: ElevatedButton(
+                onPressed: () => print('button clicked'),
+                style: ButtonStyle(
+                  minimumSize: WidgetStatePropertyAll(
+                    Size(double.infinity, 50),
+                  ),
+                  foregroundColor: WidgetStatePropertyAll(Colors.white),
+                  backgroundColor: WidgetStatePropertyAll(Colors.black),
+                  shape: WidgetStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                ),
+                child: Text('Convert', style: TextStyle(fontSize: 16)),
+              ),
             ),
           ],
         ),
